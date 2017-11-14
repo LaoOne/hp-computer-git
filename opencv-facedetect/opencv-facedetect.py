33612a7682ddf0b32685fb977f37c844dbfc725e
LaoWang <418724055@qq.com>
2017-11-14 23:03:12 +0800

    test-3

--- opencv-facedetect/opencv-facedetect.py
+++ opencv-facedetect/opencv-facedetect.py
@@ -9,7 +9,7 @@ gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 #识别人脸，得到矩形区域
 rects = detector.detectMultiScale(gray,scaleFactor=1.05,minNeighbors =7,
-	minSize=(30,30),flags = CV_HAAR_SCALE_IMAGE)
+	minSize=(30,30),flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
 #遍历list中识别到的人脸，画绿色线宽2pixcel的边框
 for (x,y,w,h) in rects:
 	cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
